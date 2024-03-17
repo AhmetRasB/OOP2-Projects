25f3ab21715ea23ec066dad5516e8b2dce141be6
Ahmet Beyhan <ahmetrasimbayhan@gmail.com>
2024-03-17 18:26:15 +0300

    .

--- OOP2-Projects/Form1.Designer.cs
+++ OOP2-Projects/Form1.Designer.cs
@@ -28,13 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Vatandaş = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Vatandaş
+            // 
+            this.Vatandaş.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.Vatandaş.Location = new System.Drawing.Point(113, 60);
+            this.Vatandaş.Name = "Vatandaş";
+            this.Vatandaş.Size = new System.Drawing.Size(151, 69);
+            this.Vatandaş.TabIndex = 0;
+            this.Vatandaş.Text = "Vatandaş";
+            this.Vatandaş.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.button2.Location = new System.Drawing.Point(441, 60);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(151, 69);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Banka";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.button1.Location = new System.Drawing.Point(207, 300);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 43);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Çek";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.button3.Location = new System.Drawing.Point(402, 300);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 43);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Yatır";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.numericUpDown1.Location = new System.Drawing.Point(160, 178);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(391, 35);
+            this.numericUpDown1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(678, 369);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Vatandaş);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Vatandaş;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
     }
 }
 
